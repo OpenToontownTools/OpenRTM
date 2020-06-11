ad9a045445603cb55a23f82cf15387c61cf268a7
Nathan S <nathanstgeorge7@gmail.com>
2020-06-11 17:15:36 -0400

    Update runRTM.bat

--- runRTM.bat
+++ runRTM.bat
@@ -1,7 +1,9 @@
+@echo off
 set PATH=lib
 set PYTHONPATH=.;lib-tk
 set TTMODELS=.
 set DMODELS=.
 set CFG_PATH=.
 set CONFIG_CONFIG=:configpath=CFG_PATH
-python.exe -i toontown\toon\RobotToonManager.py
+python -i toontown/toon/RobotToonManager.py
+pause
