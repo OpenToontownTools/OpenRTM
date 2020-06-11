c0b6d36af5239e8f0a56b974719e99759f1056ba
Nathan S <nathanstgeorge7@gmail.com>
2020-06-11 17:53:13 -0400

    general: nuke our internal python libs, this is dumb

--- runRTM.bat
+++ runRTM.bat
@@ -5,5 +5,5 @@ set TTMODELS=.
 set DMODELS=.
 set CFG_PATH=.
 set CONFIG_CONFIG=:configpath=CFG_PATH
-python.exe -i toontown/toon/RobotToonManager.py
+python.exe -m toontown.toon.RobotToonManager
 pause
