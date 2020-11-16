7a5a614c99c4698092c2cda24b1da7484fa75203
Drew C <31282527+drewc5131@users.noreply.github.com>
2020-11-15 20:49:10 -0500

    i should lisetn to my own comments

--- rtm.py
+++ rtm.py
@@ -7,11 +7,12 @@ from pandac.PandaModules import *
 
 class RTMBase(ShowBase.ShowBase):
     def __init__(self):
-        ShowBase.ShowBase.__init__(self)
-
         # Load the prc file prior to launching showbase in order
         # to have it affect window related stuff
         loadPrcFile("Configrc.prc")
+        
+        ShowBase.ShowBase.__init__(self)
+
         # Import the main dlls so we don't have to repeatedly import them everywhere
         builtins.__dict__.update(__import__('panda3d.core', fromlist=['*']).__dict__)
         builtins.__dict__.update(__import__('libotp', fromlist=['*']).__dict__)
